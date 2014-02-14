670899b194a9457d82326e924029a22392677ed1
nick <nick@wonder.com>
2014-02-15 07:49:51 +0800

    Finish micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  
   root "static_pages#home"
+  resources :users
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   
   match '/help', to:'static_pages#help', via:'get'
   match '/about', to:'static_pages#about', via:'get'
   match '/contact', to:'static_pages#contact', via:'get'
   
   match '/signup', to:'users#new', via:'get'
-  resources :users
-  resources :sessions, only: [:new,:create,:destroy]
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   
